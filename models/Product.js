a59a73f9d891adaef5b252042774a01f7a791b26
b-reddington <brandon.reddington98@gmail.com>
2023-04-18 22:47:21 -0700

    Added columns in models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,34 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          module: 'Category',
+          key: 'id',
+        }
+    }
+  }
   },
   {
     sequelize,
